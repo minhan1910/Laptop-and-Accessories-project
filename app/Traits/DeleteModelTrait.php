c34c7275535a45877a17ee088c84df4ec64041dc
An <annogo123@gmail.com>
2022-08-15 19:08:36 +0700

    refactor delete setting feature

---
+++ app/Traits/DeleteModelTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Log;
+
+trait DeleteModelTrait
+{
+    public function deleteModelTrait($id, $model)
+    {
+        try {
+            $model
+                ->find($id)
+                ->delete();
+
+            return response()->json([
+                'code' => 200,
+                'message' => 'success'
+            ], 200);
+        } catch (\Exception $e) {
+
+            Log::error('Message: ' . $e->getMessage() . '----- Line: ' . $e->getLine());
+            return response()->json([
+                'code' => 500,
+                'message' => 'fail'
+            ], 500);
+        }
+    }
+}
